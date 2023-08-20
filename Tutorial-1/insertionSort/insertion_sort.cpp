08ed5675db6836f4bfa83a6277f57bc80e4f2114
Shaurya <shaurya2agarwal@gmail.com>
2023-08-20 20:38:36 +0530

    modified

--- Tutorial-1/insertionSort/insertion_sort.cpp
+++ Tutorial-1/insertionSort/insertion_sort.cpp
@@ -13,16 +13,10 @@ std::vector<int> create_array(int size) {
 }
 
 void insertion_sort(std::vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+    
+    //WRITE YOUR CODE HERE
+
+    
 }
 
 int main() {
--- Tutorial-1/mergeSort/merge_sort.cpp
+++ Tutorial-1/mergeSort/merge_sort.cpp
@@ -13,43 +13,16 @@ std::vector<int> create_array(int size) {
 }
 
 void merge(std::vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+    
+    //WRITE YOUR CODE HERE
 
-    std::vector<int> L(n1);
-    std::vector<int> R(n2);
-
-    for (int i = 0; i < n1; ++i) {
-        L[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; ++j) {
-        R[j] = arr[mid + 1 + j];
-    }
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k++] = L[i++];
-        } else {
-            arr[k++] = R[j++];
-        }
-    }
-
-    while (i < n1) {
-        arr[k++] = L[i++];
-    }
-    while (j < n2) {
-        arr[k++] = R[j++];
-    }
 }
 
 void merge_sort_helper(std::vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        merge_sort_helper(arr, left, mid);
-        merge_sort_helper(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+    
+    //WRITE YOUR CODE HERE
+
+    
 }
 
 void merge_sort(std::vector<int>& arr) {
